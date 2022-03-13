1f5efd86e935fcc8d6d0b48526151a2ff83f9494
hollaPuppy <roki216@mail.ru>
2022-03-13 19:14:52 +0300

    Update settings.py

--- back/settings.py
+++ back/settings.py
@@ -1,6 +1,6 @@
 import os
 
-DATABASE_URL: str = os.getenv('')
+DATABASE_URL: str = os.getenv('DATABASE_URL')
 if not DATABASE_URL:
     from .config import CONFIG_DATABASE_URL
     DATABASE_URL = CONFIG_DATABASE_URL
