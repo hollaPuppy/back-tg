40d4b42dea4af881ad218da344e348cb44507da5
hollaPuppy <roki216@mail.ru>
2022-03-13 15:28:32 +0600

    2
    add first route

--- back/routes/bot.py
+++ back/routes/bot.py
@@ -4,3 +4,13 @@ from back import app, engine
 from flask import jsonify
 
 
+@app.route("/get_film_info/<film_id>", methods=["GET"])
+def get_status_user(film_id):
+    with engine.connect() as con:
+        query_film_info = f"""select film_name, film_desc, film_link
+                           from films
+                           where film_id = '{film_id}'"""
+        value_film_info = query_all(query_film_info, con)
+    return jsonify(value_film_info)
+
+
