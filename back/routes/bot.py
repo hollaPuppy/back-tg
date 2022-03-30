e060f3314a1e9f5d3b33c0a54c9c2dfb13e55ec1
hollaPuppy <roki216@mail.ru>
2022-03-30 20:31:44 +0300

    add simple registration

--- back/routes/bot.py
+++ back/routes/bot.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 from back.utils import query_all, query_first
 from back import app, engine
-from flask import jsonify
+from flask import jsonify, request
 
 
 @app.route("/get_film_info/<film_id>", methods=["GET"])
@@ -14,3 +14,21 @@ def get_status_user(film_id):
     return jsonify(value_film_info)
 
 
+@app.route("/reg", methods=["POST"])
+def reg():
+    username = request.json.get("username")
+    print(username)
+    with engine.connect() as con:
+        query_req_check = f"""select exists (
+                                select
+                                from users
+                                where username = '{username}'
+                             )"""
+        is_exists: bool = query_first(query_req_check, con)['exists']
+    if is_exists:
+        return 'User already exists', 409
+    with engine.connect() as con:
+        query_req = f"""insert into users(username, reg_date)
+                        values('{username}', (select NOW()))"""
+        con.execute(query_req)
+    return 'OK', 200
